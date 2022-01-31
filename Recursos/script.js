cd497b09fcc99dbe42b80af06c7b332e32984a3c
Zekki10 <ezequiel_vera_rodriguez@hotmail.com>
2022-01-31 11:41:10 -0300

    agregar los archivos html, css y JavaScript

---
+++ Recursos/script.js
@@ -0,0 +1,8 @@
+document.getElementById('btn-menu').addEvenListener('Click', mostrar_menu) ;
+
+nav = document.getElementById('nav');
+background_menu = document.getElementById('back_menu');
+
+function mostrar_menu() {
+	nav.style.right = "0px" ;
+}
