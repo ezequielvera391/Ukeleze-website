34139e08b1f7b53f584c118b28230f65cad3e8ee
Zekki10 <ezequiel_vera_rodriguez@hotmail.com>
2022-04-13 22:08:54 -0300

    Optimizacion del codigo

--- Recursos/script.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('btn-menu').addEvenListener('Click', mostrar_menu) ;
-
-nav = document.getElementById('nav');
-background_menu = document.getElementById('back_menu');
-
-function mostrar_menu() {
-	nav.style.right = "0px" ;
-}
